78f469aa50d50883d77ee466c5ead4d1be7ff236
zebra-lucky <zebra.lucky@gmail.com>
2018-04-09 15:15:03 +0300

    fix coinbase detection for z inputs

--- lib/wallet.py
+++ lib/wallet.py
@@ -654,7 +654,7 @@ class Abstract_Wallet(PrintError):
                     return addr
 
     def add_transaction(self, tx_hash, tx):
-        is_coinbase = tx.inputs()[0]['type'] == 'coinbase'
+        is_coinbase = len(tx.inputs()) and tx.inputs()[0]['type'] == 'coinbase'
         with self.transaction_lock:
             # add inputs
             self.txi[tx_hash] = d = {}
