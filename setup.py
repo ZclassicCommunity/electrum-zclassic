fb23570b0e6a594d50dd0156d9233a84c17d53b7
zebra-lucky <zebra.lucky@gmail.com>
2018-06-15 01:26:23 +0300

    add root repo files changes from 3.1.3

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ import platform
 import imp
 import argparse
 
+with open('contrib/requirements/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('contrib/requirements/requirements-hw.txt') as f:
+    requirements_hw = f.read().splitlines()
+
 version = imp.load_source('version', 'lib/version.py')
 
 if sys.version_info[:3] < (3, 4, 0):
@@ -21,31 +27,26 @@ if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
     parser.add_argument('--root=', dest='root_path', metavar='dir', default='/')
     opts, _ = parser.parse_known_args(sys.argv[1:])
     usr_share = os.path.join(sys.prefix, "share")
+    icons_dirname = 'pixmaps'
     if not os.access(opts.root_path + usr_share, os.W_OK) and \
        not os.access(opts.root_path, os.W_OK):
+        icons_dirname = 'icons'
         if 'XDG_DATA_HOME' in os.environ.keys():
             usr_share = os.environ['XDG_DATA_HOME']
         else:
             usr_share = os.path.expanduser('~/.local/share')
     data_files += [
         (os.path.join(usr_share, 'applications/'), ['electrum-zcash.desktop']),
-        (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum-zcash.png'])
+        (os.path.join(usr_share, icons_dirname), ['icons/electrum-zcash.png'])
     ]
 
 setup(
     name="Electrum-Zcash",
     version=version.ELECTRUM_VERSION,
-    install_requires=[
-        'pyaes>=0.1a1',
-        'ecdsa>=0.9',
-        'pbkdf2',
-        'requests',
-        'qrcode',
-        'protobuf',
-        'dnspython',
-        'jsonrpclib-pelix',
-        'PySocks>=1.6.6',
-    ],
+    install_requires=requirements,
+    extras_require={
+        'full': requirements_hw + ['pycryptodomex'],
+    },
     packages=[
         'electrum_zcash',
         'electrum_zcash_gui',
@@ -71,8 +72,9 @@ setup(
         'electrum_zcash': [
             'servers.json',
             'servers_testnet.json',
+            'servers_regtest.json',
             'currencies.json',
-            'www/index.html',
+            'checkpoints.json',
             'wordlist/*.txt',
             'locale/*/LC_MESSAGES/electrum.mo',
         ]
