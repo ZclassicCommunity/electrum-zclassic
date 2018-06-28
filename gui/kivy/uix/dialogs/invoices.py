AXErunners/electrum-axe

add gui/kivy changes from electrum 3.1.3
- Add electrum 3.1.3 changes to gui/kivy
- Get new electrum 3.1.3 gui/kivy/Readme.md
- Get new electrum 3.1.3 gui/kivy/uix/dialogs/requests.py.
- Get new electrum 3.1.3 gui/kivy/uix/dialogs/invoices.py.
- Get new electrum 3.1.3 gui/kivy/uix/dialogs/addresses.py

gui/kivy/uix/dialogs/invoices.py
@@ -1,0 +1,169 @@
+from kivy.app import App
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from decimal import Decimal
+
+Builder.load_string('''
+<InvoicesLabel@Label>
+    #color: .305, .309, .309, 1
+    text_size: self.width, None
+    halign: 'left'
+    valign: 'top'
+
+<InvoiceItem@CardItem>
+    requestor: ''
+    memo: ''
+    amount: ''
+    status: ''
+    date: ''
+    icon: 'atlas://gui/kivy/theming/light/important'
+    Image:
+        id: icon
+        source: root.icon
+        size_hint: None, 1
+        width: self.height *.54
+        mipmap: True
+    BoxLayout:
+        spacing: '8dp'
+        height: '32dp'
+        orientation: 'vertical'
+        Widget
+        InvoicesLabel:
+            text: root.requestor
+            shorten: True
+        Widget
+        InvoicesLabel:
+            text: root.memo
+            color: .699, .699, .699, 1
+            font_size: '13sp'
+            shorten: True
+        Widget
+    BoxLayout:
+        spacing: '8dp'
+        height: '32dp'
+        orientation: 'vertical'
+        Widget
+        InvoicesLabel:
+            text: root.amount
+            font_size: '15sp'
+            halign: 'right'
+            width: '110sp'
+        Widget
+        InvoicesLabel:
+            text: root.status
+            font_size: '13sp'
+            halign: 'right'
+            color: .699, .699, .699, 1
+        Widget
+
+
+<InvoicesDialog@Popup>
+    id: popup
+    title: _('Invoices')
+    BoxLayout:
+        id: box
+        orientation: 'vertical'
+        spacing: '1dp'
+        ScrollView:
+            GridLayout:
+                cols: 1
+                id: invoices_container
+                size_hint: 1, None
+                height: self.minimum_height
+                spacing: '2dp'
+                padding: '12dp'
+''')
+
+from kivy.properties import BooleanProperty
+from electrum_dash_gui.kivy.i18n import _
+from electrum_dash.util import format_time
+from electrum_dash.paymentrequest import PR_UNPAID, PR_PAID, PR_UNKNOWN, PR_EXPIRED
+from electrum_dash_gui.kivy.uix.context_menu import ContextMenu
+
+invoice_text = {
+    PR_UNPAID:_('Pending'),
+    PR_UNKNOWN:_('Unknown'),
+    PR_PAID:_('Paid'),
+    PR_EXPIRED:_('Expired')
+}
+pr_icon = {
+    PR_UNPAID: 'atlas://gui/kivy/theming/light/important',
+    PR_UNKNOWN: 'atlas://gui/kivy/theming/light/important',
+    PR_PAID: 'atlas://gui/kivy/theming/light/confirmed',
+    PR_EXPIRED: 'atlas://gui/kivy/theming/light/close'
+}
+
+
+class InvoicesDialog(Factory.Popup):
+
+    def __init__(self, app, screen, callback):
+        Factory.Popup.__init__(self)
+        self.app = app
+        self.screen = screen
+        self.callback = callback
+        self.cards = {}
+        self.context_menu = None
+
+    def get_card(self, pr):
+        key = pr.get_id()
+        ci = self.cards.get(key)
+        if ci is None:
+            ci = Factory.InvoiceItem()
+            ci.key = key
+            ci.screen = self
+            self.cards[key] = ci
+        ci.requestor = pr.get_requestor()
+        ci.memo = pr.get_memo()
+        amount = pr.get_amount()
+        if amount:
+            ci.amount = self.app.format_amount_and_units(amount)
+            status = self.app.wallet.invoices.get_status(ci.key)
+            ci.status = invoice_text[status]
+            ci.icon = pr_icon[status]
+        else:
+            ci.amount = _('No Amount')
+            ci.status = ''
+        exp = pr.get_expiration_date()
+        ci.date = format_time(exp) if exp else _('Never')
+        return ci
+
+    def update(self):
+        self.menu_actions = [('Pay', self.do_pay), ('Details', self.do_view), ('Delete', self.do_delete)]
+        invoices_list = self.ids.invoices_container
+        invoices_list.clear_widgets()
+        _list = self.app.wallet.invoices.sorted_list()
+        for pr in _list:
+            ci = self.get_card(pr)
+            invoices_list.add_widget(ci)
+
+    def do_pay(self, obj):
+        self.hide_menu()
+        self.dismiss()
+        pr = self.app.wallet.invoices.get(obj.key)
+        self.app.on_pr(pr)
+
+    def do_view(self, obj):
+        pr = self.app.wallet.invoices.get(obj.key)
+        pr.verify(self.app.wallet.contacts)
+        self.app.show_pr_details(pr.get_dict(), obj.status, True)
+
+    def do_delete(self, obj):
+        from .question import Question
+        def cb(result):
+            if result:
+                self.app.wallet.invoices.remove(obj.key)
+            self.hide_menu()
+            self.update()
+        d = Question(_('Delete invoice?'), cb)
+        d.open()
+
+    def show_menu(self, obj):
+        self.hide_menu()
+        self.context_menu = ContextMenu(obj, self.menu_actions)
+        self.ids.box.add_widget(self.context_menu)
+
+    def hide_menu(self):
+        if self.context_menu is not None:
+            self.ids.box.remove_widget(self.context_menu)
+            self.context_menu = None

gui/kivy/uix/dialogs/password_dialog.py
@@ -5,35 +5,42 @@ from kivy.lang import Builder
 from decimal import Decimal
 from kivy.clock import Clock
 
+from electrum_dash.util import InvalidPassword
+from electrum_dash_gui.kivy.i18n import _
+
 Builder.load_string('''
 
 <PasswordDialog@Popup>
     id: popup
-    title: _('PIN Code')
+    title: 'Electrum-DASH'
     message: ''
-    size_hint: 0.9, 0.9
     BoxLayout:
+        size_hint: 1, 1
         orientation: 'vertical'
         Widget:
-            size_hint: 1, 1
+            size_hint: 1, 0.05
         Label:
+            font_size: '20dp'
             text: root.message
             text_size: self.width, None
             size: self.texture_size
         Widget:
-            size_hint: 1, 1
+            size_hint: 1, 0.05
         Label:
             id: a
-            text: ' * '*len(kb.password) + ' o '*(6-len(kb.password))
+            font_size: '50dp'
+            text: '*'*len(kb.password) + '-'*(6-len(kb.password))
+            size: self.texture_size
         Widget:
-            size_hint: 1, 1
+            size_hint: 1, 0.05
         GridLayout:
             id: kb
+            size_hint: 1, None
+            height: self.minimum_height
             update_amount: popup.update_password
             password: ''
             on_password: popup.on_password(self.password)
-            size_hint: 1, None
-            height: '200dp'
+            spacing: '2dp'
             cols: 3
             KButton:
                 text: '1'
@@ -59,30 +66,44 @@ Builder.load_string('''
                 text: '0'
             KButton:
                 text: '<'
-        BoxLayout:
-            size_hint: 1, None
-            height: '48dp'
-            Widget:
-                size_hint: 0.5, None
-            Button:
-                size_hint: 0.5, None
-                height: '48dp'
-                text: _('Cancel')
-                on_release:
-                    popup.dismiss()
-                    popup.callback(None)
 ''')
 
 
 class PasswordDialog(Factory.Popup):
 
-    #def __init__(self, message, callback):
-    #    Factory.Popup.__init__(self)
-
-    def init(self, message, callback):
+    def init(self, app, wallet, message, on_success, on_failure, is_change=0):
+        self.app = app
+        self.wallet = wallet
         self.message = message
-        self.callback = callback
+        self.on_success = on_success
+        self.on_failure = on_failure
         self.ids.kb.password = ''
+        self.success = False
+        self.is_change = is_change
+        self.pw = None
+        self.new_password = None
+        self.title = 'Electrum-DASH' + ('  -  ' + self.wallet.basename() if self.wallet else '')
+
+    def check_password(self, password):
+        if self.is_change > 1:
+            return True
+        try:
+            self.wallet.check_password(password)
+            return True
+        except InvalidPassword as e:
+            return False
+
+    def on_dismiss(self):
+        if not self.success:
+            if self.on_failure:
+                self.on_failure()
+            else:
+                # keep dialog open
+                return True
+        else:
+            if self.on_success:
+                args = (self.pw, self.new_password) if self.is_change else (self.pw,)
+                Clock.schedule_once(lambda dt: self.on_success(*args), 0.1)
 
     def update_password(self, c):
         kb = self.ids.kb
@@ -97,5 +118,25 @@ class PasswordDialog(Factory.Popup):
 
     def on_password(self, pw):
         if len(pw) == 6:
-            self.dismiss()
-            Clock.schedule_once(lambda dt: self.callback(pw), 0.1)
+            if self.check_password(pw):
+                if self.is_change == 0:
+                    self.success = True
+                    self.pw = pw
+                    self.message = _('Please wait...')
+                    self.dismiss()
+                elif self.is_change == 1:
+                    self.pw = pw
+                    self.message = _('Enter new PIN')
+                    self.ids.kb.password = ''
+                    self.is_change = 2
+                elif self.is_change == 2:
+                    self.new_password = pw
+                    self.message = _('Confirm new PIN')
+                    self.ids.kb.password = ''
+                    self.is_change = 3
+                elif self.is_change == 3:
+                    self.success = pw == self.new_password
+                    self.dismiss()
+            else:
+                self.app.show_error(_('Wrong PIN'))
+                self.ids.kb.password = ''

ZclassicCommunity/electrum-zclassic

gui/kivy/uix/dialogs/invoices.py
@@ -1,0 +1,169 @@
+from kivy.app import App
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from decimal import Decimal
+
+Builder.load_string('''
+<InvoicesLabel@Label>
+    #color: .305, .309, .309, 1
+    text_size: self.width, None
+    halign: 'left'
+    valign: 'top'
+
+<InvoiceItem@CardItem>
+    requestor: ''
+    memo: ''
+    amount: ''
+    status: ''
+    date: ''
+    icon: 'atlas://gui/kivy/theming/light/important'
+    Image:
+        id: icon
+        source: root.icon
+        size_hint: None, 1
+        width: self.height *.54
+        mipmap: True
+    BoxLayout:
+        spacing: '8dp'
+        height: '32dp'
+        orientation: 'vertical'
+        Widget
+        InvoicesLabel:
+            text: root.requestor
+            shorten: True
+        Widget
+        InvoicesLabel:
+            text: root.memo
+            color: .699, .699, .699, 1
+            font_size: '13sp'
+            shorten: True
+        Widget
+    BoxLayout:
+        spacing: '8dp'
+        height: '32dp'
+        orientation: 'vertical'
+        Widget
+        InvoicesLabel:
+            text: root.amount
+            font_size: '15sp'
+            halign: 'right'
+            width: '110sp'
+        Widget
+        InvoicesLabel:
+            text: root.status
+            font_size: '13sp'
+            halign: 'right'
+            color: .699, .699, .699, 1
+        Widget
+
+
+<InvoicesDialog@Popup>
+    id: popup
+    title: _('Invoices')
+    BoxLayout:
+        id: box
+        orientation: 'vertical'
+        spacing: '1dp'
+        ScrollView:
+            GridLayout:
+                cols: 1
+                id: invoices_container
+                size_hint: 1, None
+                height: self.minimum_height
+                spacing: '2dp'
+                padding: '12dp'
+''')
+
+from kivy.properties import BooleanProperty
+from electrum_zcash_gui.kivy.i18n import _
+from electrum_zcash.util import format_time
+from electrum_zcash.paymentrequest import PR_UNPAID, PR_PAID, PR_UNKNOWN, PR_EXPIRED
+from electrum_zcash_gui.kivy.uix.context_menu import ContextMenu
+
+invoice_text = {
+    PR_UNPAID:_('Pending'),
+    PR_UNKNOWN:_('Unknown'),
+    PR_PAID:_('Paid'),
+    PR_EXPIRED:_('Expired')
+}
+pr_icon = {
+    PR_UNPAID: 'atlas://gui/kivy/theming/light/important',
+    PR_UNKNOWN: 'atlas://gui/kivy/theming/light/important',
+    PR_PAID: 'atlas://gui/kivy/theming/light/confirmed',
+    PR_EXPIRED: 'atlas://gui/kivy/theming/light/close'
+}
+
+
+class InvoicesDialog(Factory.Popup):
+
+    def __init__(self, app, screen, callback):
+        Factory.Popup.__init__(self)
+        self.app = app
+        self.screen = screen
+        self.callback = callback
+        self.cards = {}
+        self.context_menu = None
+
+    def get_card(self, pr):
+        key = pr.get_id()
+        ci = self.cards.get(key)
+        if ci is None:
+            ci = Factory.InvoiceItem()
+            ci.key = key
+            ci.screen = self
+            self.cards[key] = ci
+        ci.requestor = pr.get_requestor()
+        ci.memo = pr.get_memo()
+        amount = pr.get_amount()
+        if amount:
+            ci.amount = self.app.format_amount_and_units(amount)
+            status = self.app.wallet.invoices.get_status(ci.key)
+            ci.status = invoice_text[status]
+            ci.icon = pr_icon[status]
+        else:
+            ci.amount = _('No Amount')
+            ci.status = ''
+        exp = pr.get_expiration_date()
+        ci.date = format_time(exp) if exp else _('Never')
+        return ci
+
+    def update(self):
+        self.menu_actions = [('Pay', self.do_pay), ('Details', self.do_view), ('Delete', self.do_delete)]
+        invoices_list = self.ids.invoices_container
+        invoices_list.clear_widgets()
+        _list = self.app.wallet.invoices.sorted_list()
+        for pr in _list:
+            ci = self.get_card(pr)
+            invoices_list.add_widget(ci)
+
+    def do_pay(self, obj):
+        self.hide_menu()
+        self.dismiss()
+        pr = self.app.wallet.invoices.get(obj.key)
+        self.app.on_pr(pr)
+
+    def do_view(self, obj):
+        pr = self.app.wallet.invoices.get(obj.key)
+        pr.verify(self.app.wallet.contacts)
+        self.app.show_pr_details(pr.get_dict(), obj.status, True)
+
+    def do_delete(self, obj):
+        from .question import Question
+        def cb(result):
+            if result:
+                self.app.wallet.invoices.remove(obj.key)
+            self.hide_menu()
+            self.update()
+        d = Question(_('Delete invoice?'), cb)
+        d.open()
+
+    def show_menu(self, obj):
+        self.hide_menu()
+        self.context_menu = ContextMenu(obj, self.menu_actions)
+        self.ids.box.add_widget(self.context_menu)
+
+    def hide_menu(self):
+        if self.context_menu is not None:
+            self.ids.box.remove_widget(self.context_menu)
+            self.context_menu = None

gui/kivy/uix/dialogs/password_dialog.py
@@ -5,35 +5,42 @@ from kivy.lang import Builder
 from decimal import Decimal
 from kivy.clock import Clock
 
+from electrum_zcash.util import InvalidPassword
+from electrum_zcash_gui.kivy.i18n import _
+
 Builder.load_string('''
 
 <PasswordDialog@Popup>
     id: popup
-    title: _('PIN Code')
+    title: 'Electrum-Zcash'
     message: ''
-    size_hint: 0.9, 0.9
     BoxLayout:
+        size_hint: 1, 1
         orientation: 'vertical'
         Widget:
-            size_hint: 1, 1
+            size_hint: 1, 0.05
         Label:
+            font_size: '20dp'
             text: root.message
             text_size: self.width, None
             size: self.texture_size
         Widget:
-            size_hint: 1, 1
+            size_hint: 1, 0.05
         Label:
             id: a
-            text: ' * '*len(kb.password) + ' o '*(6-len(kb.password))
+            font_size: '50dp'
+            text: '*'*len(kb.password) + '-'*(6-len(kb.password))
+            size: self.texture_size
         Widget:
-            size_hint: 1, 1
+            size_hint: 1, 0.05
         GridLayout:
             id: kb
+            size_hint: 1, None
+            height: self.minimum_height
             update_amount: popup.update_password
             password: ''
             on_password: popup.on_password(self.password)
-            size_hint: 1, None
-            height: '200dp'
+            spacing: '2dp'
             cols: 3
             KButton:
                 text: '1'
@@ -59,30 +66,44 @@ Builder.load_string('''
                 text: '0'
             KButton:
                 text: '<'
-        BoxLayout:
-            size_hint: 1, None
-            height: '48dp'
-            Widget:
-                size_hint: 0.5, None
-            Button:
-                size_hint: 0.5, None
-                height: '48dp'
-                text: _('Cancel')
-                on_release:
-                    popup.dismiss()
-                    popup.callback(None)
 ''')
 
 
 class PasswordDialog(Factory.Popup):
 
-    #def __init__(self, message, callback):
-    #    Factory.Popup.__init__(self)
-
-    def init(self, message, callback):
+    def init(self, app, wallet, message, on_success, on_failure, is_change=0):
+        self.app = app
+        self.wallet = wallet
         self.message = message
-        self.callback = callback
+        self.on_success = on_success
+        self.on_failure = on_failure
         self.ids.kb.password = ''
+        self.success = False
+        self.is_change = is_change
+        self.pw = None
+        self.new_password = None
+        self.title = 'Electrum-Zcash' + ('  -  ' + self.wallet.basename() if self.wallet else '')
+
+    def check_password(self, password):
+        if self.is_change > 1:
+            return True
+        try:
+            self.wallet.check_password(password)
+            return True
+        except InvalidPassword as e:
+            return False
+
+    def on_dismiss(self):
+        if not self.success:
+            if self.on_failure:
+                self.on_failure()
+            else:
+                # keep dialog open
+                return True
+        else:
+            if self.on_success:
+                args = (self.pw, self.new_password) if self.is_change else (self.pw,)
+                Clock.schedule_once(lambda dt: self.on_success(*args), 0.1)
 
     def update_password(self, c):
         kb = self.ids.kb
@@ -97,5 +118,25 @@ class PasswordDialog(Factory.Popup):
 
     def on_password(self, pw):
         if len(pw) == 6:
-            self.dismiss()
-            Clock.schedule_once(lambda dt: self.callback(pw), 0.1)
+            if self.check_password(pw):
+                if self.is_change == 0:
+                    self.success = True
+                    self.pw = pw
+                    self.message = _('Please wait...')
+                    self.dismiss()
+                elif self.is_change == 1:
+                    self.pw = pw
+                    self.message = _('Enter new PIN')
+                    self.ids.kb.password = ''
+                    self.is_change = 2
+                elif self.is_change == 2:
+                    self.new_password = pw
+                    self.message = _('Confirm new PIN')
+                    self.ids.kb.password = ''
+                    self.is_change = 3
+                elif self.is_change == 3:
+                    self.success = pw == self.new_password
+                    self.dismiss()
+            else:
+                self.app.show_error(_('Wrong PIN'))
+                self.ids.kb.password = ''