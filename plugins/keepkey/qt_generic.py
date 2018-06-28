AXErunners/electrum-axe

add plugins changes from electrum 3.1.3

plugins/keepkey/qt_generic.py
@@ -194,7 +194,7 @@ class QtPlugin(QtPluginBase):
         if type(keystore) == self.keystore_class and len(addrs) == 1:
             def show_address():
                 keystore.thread.add(partial(self.show_address, wallet, addrs[0]))
-            menu.addAction(_("Show on %s") % self.device, show_address)
+            menu.addAction(_("Show on {}").format(self.device), show_address)
 
     def show_settings_dialog(self, window, keystore):
         device_id = self.choose_device(window, keystore)
@@ -227,7 +227,7 @@ class QtPlugin(QtPluginBase):
             bg = QButtonGroup()
             for i, count in enumerate([12, 18, 24]):
                 rb = QRadioButton(gb)
-                rb.setText(_("%d words") % count)
+                rb.setText(_("{} words").format(count))
                 bg.addButton(rb)
                 bg.setId(rb, i)
                 hbox1.addWidget(rb)
@@ -250,7 +250,7 @@ class QtPlugin(QtPluginBase):
             vbox.addWidget(QLabel(msg))
             vbox.addWidget(text)
             pin = QLineEdit()
-            pin.setValidator(QRegExpValidator(QRegExp('[1-9]{0,10}')))
+            pin.setValidator(QRegExpValidator(QRegExp('[1-9]{0,9}')))
             pin.setMaximumWidth(100)
             hbox_pin = QHBoxLayout()
             hbox_pin.addWidget(QLabel(_("Enter your PIN (digits 1-9):")))
@@ -292,7 +292,7 @@ class SettingsDialog(WindowModalDialog):
     their PIN.'''
 
     def __init__(self, window, plugin, keystore, device_id):
-        title = _("%s Settings") % plugin.device
+        title = _("{} Settings").format(plugin.device)
         super(SettingsDialog, self).__init__(window, title)
         self.setMaximumWidth(540)
 
@@ -457,9 +457,9 @@ class SettingsDialog(WindowModalDialog):
         settings_glayout = QGridLayout()
 
         # Settings tab - Label
-        label_msg = QLabel(_("Name this %s.  If you have mutiple devices "
+        label_msg = QLabel(_("Name this {}.  If you have multiple devices "
                              "their labels help distinguish them.")
-                           % plugin.device)
+                           .format(plugin.device))
         label_msg.setWordWrap(True)
         label_label = QLabel(_("Device Label"))
         label_edit = QLineEdit()
@@ -482,7 +482,7 @@ class SettingsDialog(WindowModalDialog):
         pin_msg = QLabel(_("PIN protection is strongly recommended.  "
                            "A PIN is your only protection against someone "
                            "stealing your Dash coins if they obtain physical "
-                           "access to your %s.") % plugin.device)
+                           "access to your {}.").format(plugin.device))
         pin_msg.setWordWrap(True)
         pin_msg.setStyleSheet("color: red")
         settings_glayout.addWidget(pin_msg, 3, 1, 1, -1)
@@ -497,8 +497,8 @@ class SettingsDialog(WindowModalDialog):
             homescreen_clear_button.clicked.connect(clear_homescreen)
             homescreen_msg = QLabel(_("You can set the homescreen on your "
                                       "device to personalize it.  You must "
-                                      "choose a %d x %d monochrome black and "
-                                      "white image.") % (hs_rows, hs_cols))
+                                      "choose a {} x {} monochrome black and "
+                                      "white image.").format(hs_rows, hs_cols))
             homescreen_msg.setWordWrap(True)
             settings_glayout.addWidget(homescreen_label, 4, 0)
             settings_glayout.addWidget(homescreen_change_button, 4, 1)
@@ -541,7 +541,7 @@ class SettingsDialog(WindowModalDialog):
         clear_pin_button.clicked.connect(clear_pin)
         clear_pin_warning = QLabel(
             _("If you disable your PIN, anyone with physical access to your "
-              "%s device can spend your Dash coins.") % plugin.device)
+              "{} device can spend your Dash coins.").format(plugin.device))
         clear_pin_warning.setWordWrap(True)
         clear_pin_warning.setStyleSheet("color: red")
         advanced_glayout.addWidget(clear_pin_button, 0, 2)

plugins/ledger/auth2fa.py
@@ -1,16 +1,24 @@
+import os
+import hashlib
+import logging
+import json
+import copy
 from binascii import hexlify, unhexlify
+
+import websocket
 
 from PyQt5.Qt import QDialog, QLineEdit, QTextEdit, QVBoxLayout, QLabel
 import PyQt5.QtCore as QtCore
 from PyQt5.QtWidgets import *
 
+from btchip.btchip import *
+
 from electrum_dash.i18n import _
 from electrum_dash_gui.qt.util import *
 from electrum_dash.util import print_msg
-
-import os, hashlib, websocket, logging, json, copy
+from electrum_dash import constants, bitcoin
 from electrum_dash_gui.qt.qrcodewidget import QRCodeWidget
-from btchip.btchip import *
+
 
 DEBUG = False
 
@@ -24,7 +32,7 @@ helpTxt = [_("Your Ledger Wallet wants to tell you a one-time PIN code.<br><br>"
         _("Waiting for authentication on your mobile phone"),
         _("Transaction accepted by mobile phone. Waiting for confirmation."),
         _("Click Pair button to begin pairing a mobile phone."),
-        _("Scan this QR code with your LedgerWallet phone app to pair it with this Ledger device.<br>" 
+        _("Scan this QR code with your Ledger Wallet phone app to pair it with this Ledger device.<br>" 
             "To complete pairing you will need your security card to answer a challenge." )
         ]
 
@@ -37,7 +45,7 @@ class LedgerAuthDialog(QDialog):
         self.handler = handler
         self.txdata = data
         self.idxs = self.txdata['keycardData'] if self.txdata['confirmationType'] > 1 else ''
-        self.setMinimumWidth(600)
+        self.setMinimumWidth(650)
         self.setWindowTitle(_("Ledger Wallet Authentication"))
         self.cfg = copy.deepcopy(self.handler.win.wallet.get_keystore().cfg)
         self.dongle = self.handler.win.wallet.get_keystore().get_client().dongle
@@ -110,17 +118,23 @@ class LedgerAuthDialog(QDialog):
         card = QVBoxLayout()
         self.cardbox.setLayout(card)
         self.addrtext = QTextEdit()
-        self.addrtext.setStyleSheet("QTextEdit { color:blue; background-color:lightgray; padding:15px 10px; border:none; font-size:20pt; }")
+        self.addrtext.setStyleSheet("QTextEdit { color:blue; background-color:lightgray; padding:15px 10px; border:none; font-size:20pt; font-family:monospace; }")
         self.addrtext.setReadOnly(True)
-        self.addrtext.setMaximumHeight(120)
+        self.addrtext.setMaximumHeight(130)
         card.addWidget(self.addrtext)
         
         def pin_changed(s):
             if len(s) < len(self.idxs):
                 i = self.idxs[len(s)]
                 addr = self.txdata['address']
-                addr = addr[:i] + '<u><b>' + addr[i:i+1] + '</u></b>' + addr[i+1:]
-                self.addrtext.setHtml(str(addr))
+                if not constants.net.TESTNET:
+                    text = addr[:i] + '<u><b>' + addr[i:i+1] + '</u></b>' + addr[i+1:]
+                else:
+                    # pin needs to be created from mainnet address
+                    addr_mainnet = bitcoin.script_to_address(bitcoin.address_to_script(addr), net=constants.BitcoinMainnet)
+                    addr_mainnet = addr_mainnet[:i] + '<u><b>' + addr_mainnet[i:i+1] + '</u></b>' + addr_mainnet[i+1:]
+                    text = str(addr) + '\n' + str(addr_mainnet)
+                self.addrtext.setHtml(str(text))
             else:
                 self.addrtext.setHtml(_("Press Enter"))
                 
@@ -164,7 +178,7 @@ class LedgerAuthDialog(QDialog):
             if not self.cfg['pair']:
                 self.modes.addItem(_("Mobile - Not paired")) 
             else:
-                self.modes.addItem(_("Mobile - %s") % self.cfg['pair'][1]) 
+                self.modes.addItem(_("Mobile - {}").format(self.cfg['pair'][1]))
         self.modes.blockSignals(False)
         
     def update_dlg(self):
@@ -179,8 +193,8 @@ class LedgerAuthDialog(QDialog):
         self.pinbox.setVisible(self.cfg['mode'] == 0)
         self.cardbox.setVisible(self.cfg['mode'] == 1)
         self.pintxt.setFocus(True) if self.cfg['mode'] == 0 else self.cardtxt.setFocus(True)
-        self.setMaximumHeight(200)
-        
+        self.setMaximumHeight(400)
+
     def do_pairing(self):
         rng = os.urandom(16)
         pairID = (hexlify(rng) + hexlify(hashlib.sha256(rng).digest()[0:1])).decode('utf-8')
@@ -338,11 +352,7 @@ class LedgerWebSocket(QThread):
             ws.send( self.txreq )
             debug_msg("Req Sent", self.txreq)
 
+
 def debug_msg(*args):
     if DEBUG:
         print_msg(*args)        
-
-    
-        
-        
-        

plugins/ledger/qt.py
@@ -1,13 +1,13 @@
-import threading
-
-from PyQt5.Qt import QInputDialog, QLineEdit, QVBoxLayout, QLabel
+#from btchip.btchipPersoWizard import StartBTChipPersoDialog
 
 from electrum_dash.i18n import _
-from .ledger import LedgerPlugin
-from ..hw_wallet.qt import QtHandlerBase, QtPluginBase
+from electrum_dash.plugins import hook
+from electrum_dash.wallet import Standard_Wallet
 from electrum_dash_gui.qt.util import *
 
-#from btchip.btchipPersoWizard import StartBTChipPersoDialog
+from .ledger import LedgerPlugin
+from ..hw_wallet.qt import QtHandlerBase, QtPluginBase
+
 
 class Plugin(LedgerPlugin, QtPluginBase):
     icon_unpaired = ":icons/ledger_unpaired.png"
@@ -15,6 +15,16 @@ class Plugin(LedgerPlugin, QtPluginBase):
 
     def create_handler(self, window):
         return Ledger_Handler(window)
+
+    @hook
+    def receive_menu(self, menu, addrs, wallet):
+        if type(wallet) is not Standard_Wallet:
+            return
+        keystore = wallet.get_keystore()
+        if type(keystore) == self.keystore_class and len(addrs) == 1:
+            def show_address():
+                keystore.thread.add(partial(self.show_address, wallet, addrs[0]))
+            menu.addAction(_("Show on Ledger"), show_address)
 
 class Ledger_Handler(QtHandlerBase):
     setup_signal = pyqtSignal()
@@ -65,11 +75,7 @@ class Ledger_Handler(QtHandlerBase):
         return 
         
     def setup_dialog(self):
+        self.show_error(_('Initialization of Ledger HW devices is currently disabled.'))
+        return
         dialog = StartBTChipPersoDialog()
         dialog.exec_()
-
-
-        
-        
-        
-        

plugins/trezor/transport.py
@@ -1,0 +1,95 @@
+from electrum_dash.util import PrintError
+
+
+class TrezorTransport(PrintError):
+
+    @staticmethod
+    def all_transports():
+        """Reimplemented trezorlib.transport.all_transports so that we can
+        enable/disable specific transports.
+        """
+        try:
+            # only to detect trezorlib version
+            from trezorlib.transport import all_transports
+        except ImportError:
+            # old trezorlib. compat for trezorlib < 0.9.2
+            transports = []
+            #try:
+            #    from trezorlib.transport_bridge import BridgeTransport
+            #    transports.append(BridgeTransport)
+            #except BaseException:
+            #    pass
+            try:
+                from trezorlib.transport_hid import HidTransport
+                transports.append(HidTransport)
+            except BaseException:
+                pass
+            try:
+                from trezorlib.transport_udp import UdpTransport
+                transports.append(UdpTransport)
+            except BaseException:
+                pass
+            try:
+                from trezorlib.transport_webusb import WebUsbTransport
+                transports.append(WebUsbTransport)
+            except BaseException:
+                pass
+        else:
+            # new trezorlib.
+            transports = []
+            #try:
+            #    from trezorlib.transport.bridge import BridgeTransport
+            #    transports.append(BridgeTransport)
+            #except BaseException:
+            #    pass
+            try:
+                from trezorlib.transport.hid import HidTransport
+                transports.append(HidTransport)
+            except BaseException:
+                pass
+            try:
+                from trezorlib.transport.udp import UdpTransport
+                transports.append(UdpTransport)
+            except BaseException:
+                pass
+            try:
+                from trezorlib.transport.webusb import WebUsbTransport
+                transports.append(WebUsbTransport)
+            except BaseException:
+                pass
+            return transports
+        return transports
+
+    def enumerate_devices(self):
+        """Just like trezorlib.transport.enumerate_devices,
+        but with exception catching, so that transports can fail separately.
+        """
+        devices = []
+        for transport in self.all_transports():
+            try:
+                new_devices = transport.enumerate()
+            except BaseException as e:
+                self.print_error('enumerate failed for {}. error {}'
+                                 .format(transport.__name__, str(e)))
+            else:
+                devices.extend(new_devices)
+        return devices
+
+    def get_transport(self, path=None):
+        """Reimplemented trezorlib.transport.get_transport,
+        (1) for old trezorlib
+        (2) to be able to disable specific transports
+        (3) to call our own enumerate_devices that catches exceptions
+        """
+        if path is None:
+            try:
+                return self.enumerate_devices()[0]
+            except IndexError:
+                raise Exception("No TREZOR device found") from None
+
+        def match_prefix(a, b):
+            return a.startswith(b) or b.startswith(a)
+        transports = [t for t in self.all_transports() if match_prefix(path, t.PATH_PREFIX)]
+        if transports:
+            return transports[0].find_by_path(path)
+        raise Exception("Unknown path prefix '%s'" % path)

ZclassicCommunity/electrum-zclassic

plugins/keepkey/qt_generic.py
@@ -194,7 +194,7 @@ class QtPlugin(QtPluginBase):
         if type(keystore) == self.keystore_class and len(addrs) == 1:
             def show_address():
                 keystore.thread.add(partial(self.show_address, wallet, addrs[0]))
-            menu.addAction(_("Show on %s") % self.device, show_address)
+            menu.addAction(_("Show on {}").format(self.device), show_address)
 
     def show_settings_dialog(self, window, keystore):
         device_id = self.choose_device(window, keystore)
@@ -227,7 +227,7 @@ class QtPlugin(QtPluginBase):
             bg = QButtonGroup()
             for i, count in enumerate([12, 18, 24]):
                 rb = QRadioButton(gb)
-                rb.setText(_("%d words") % count)
+                rb.setText(_("{} words").format(count))
                 bg.addButton(rb)
                 bg.setId(rb, i)
                 hbox1.addWidget(rb)
@@ -250,7 +250,7 @@ class QtPlugin(QtPluginBase):
             vbox.addWidget(QLabel(msg))
             vbox.addWidget(text)
             pin = QLineEdit()
-            pin.setValidator(QRegExpValidator(QRegExp('[1-9]{0,10}')))
+            pin.setValidator(QRegExpValidator(QRegExp('[1-9]{0,9}')))
             pin.setMaximumWidth(100)
             hbox_pin = QHBoxLayout()
             hbox_pin.addWidget(QLabel(_("Enter your PIN (digits 1-9):")))
@@ -292,7 +292,7 @@ class SettingsDialog(WindowModalDialog):
     their PIN.'''
 
     def __init__(self, window, plugin, keystore, device_id):
-        title = _("%s Settings") % plugin.device
+        title = _("{} Settings").format(plugin.device)
         super(SettingsDialog, self).__init__(window, title)
         self.setMaximumWidth(540)
 
@@ -457,9 +457,9 @@ class SettingsDialog(WindowModalDialog):
         settings_glayout = QGridLayout()
 
         # Settings tab - Label
-        label_msg = QLabel(_("Name this %s.  If you have mutiple devices "
+        label_msg = QLabel(_("Name this {}.  If you have multiple devices "
                              "their labels help distinguish them.")
-                           % plugin.device)
+                           .format(plugin.device))
         label_msg.setWordWrap(True)
         label_label = QLabel(_("Device Label"))
         label_edit = QLineEdit()
@@ -482,7 +482,7 @@ class SettingsDialog(WindowModalDialog):
         pin_msg = QLabel(_("PIN protection is strongly recommended.  "
                            "A PIN is your only protection against someone "
                            "stealing your Zcash coins if they obtain physical "
-                           "access to your %s.") % plugin.device)
+                           "access to your {}.").format(plugin.device))
         pin_msg.setWordWrap(True)
         pin_msg.setStyleSheet("color: red")
         settings_glayout.addWidget(pin_msg, 3, 1, 1, -1)
@@ -497,8 +497,8 @@ class SettingsDialog(WindowModalDialog):
             homescreen_clear_button.clicked.connect(clear_homescreen)
             homescreen_msg = QLabel(_("You can set the homescreen on your "
                                       "device to personalize it.  You must "
-                                      "choose a %d x %d monochrome black and "
-                                      "white image.") % (hs_rows, hs_cols))
+                                      "choose a {} x {} monochrome black and "
+                                      "white image.").format(hs_rows, hs_cols))
             homescreen_msg.setWordWrap(True)
             settings_glayout.addWidget(homescreen_label, 4, 0)
             settings_glayout.addWidget(homescreen_change_button, 4, 1)
@@ -541,7 +541,7 @@ class SettingsDialog(WindowModalDialog):
         clear_pin_button.clicked.connect(clear_pin)
         clear_pin_warning = QLabel(
             _("If you disable your PIN, anyone with physical access to your "
-              "%s device can spend your Zcash coins.") % plugin.device)
+              "{} device can spend your Zcash coins.").format(plugin.device))
         clear_pin_warning.setWordWrap(True)
         clear_pin_warning.setStyleSheet("color: red")
         advanced_glayout.addWidget(clear_pin_button, 0, 2)

plugins/ledger/auth2fa.py
@@ -1,16 +1,24 @@
+import os
+import hashlib
+import logging
+import json
+import copy
 from binascii import hexlify, unhexlify
+
+import websocket
 
 from PyQt5.Qt import QDialog, QLineEdit, QTextEdit, QVBoxLayout, QLabel
 import PyQt5.QtCore as QtCore
 from PyQt5.QtWidgets import *
 
+from btchip.btchip import *
+
 from electrum_zcash.i18n import _
 from electrum_zcash_gui.qt.util import *
 from electrum_zcash.util import print_msg
-
-import os, hashlib, websocket, logging, json, copy
+from electrum_zcash import constants, bitcoin
 from electrum_zcash_gui.qt.qrcodewidget import QRCodeWidget
-from btchip.btchip import *
+
 
 DEBUG = False
 
@@ -24,7 +32,7 @@ helpTxt = [_("Your Ledger Wallet wants to tell you a one-time PIN code.<br><br>"
         _("Waiting for authentication on your mobile phone"),
         _("Transaction accepted by mobile phone. Waiting for confirmation."),
         _("Click Pair button to begin pairing a mobile phone."),
-        _("Scan this QR code with your LedgerWallet phone app to pair it with this Ledger device.<br>" 
+        _("Scan this QR code with your Ledger Wallet phone app to pair it with this Ledger device.<br>" 
             "To complete pairing you will need your security card to answer a challenge." )
         ]
 
@@ -37,7 +45,7 @@ class LedgerAuthDialog(QDialog):
         self.handler = handler
         self.txdata = data
         self.idxs = self.txdata['keycardData'] if self.txdata['confirmationType'] > 1 else ''
-        self.setMinimumWidth(600)
+        self.setMinimumWidth(650)
         self.setWindowTitle(_("Ledger Wallet Authentication"))
         self.cfg = copy.deepcopy(self.handler.win.wallet.get_keystore().cfg)
         self.dongle = self.handler.win.wallet.get_keystore().get_client().dongle
@@ -110,17 +118,23 @@ class LedgerAuthDialog(QDialog):
         card = QVBoxLayout()
         self.cardbox.setLayout(card)
         self.addrtext = QTextEdit()
-        self.addrtext.setStyleSheet("QTextEdit { color:blue; background-color:lightgray; padding:15px 10px; border:none; font-size:20pt; }")
+        self.addrtext.setStyleSheet("QTextEdit { color:blue; background-color:lightgray; padding:15px 10px; border:none; font-size:20pt; font-family:monospace; }")
         self.addrtext.setReadOnly(True)
-        self.addrtext.setMaximumHeight(120)
+        self.addrtext.setMaximumHeight(130)
         card.addWidget(self.addrtext)
         
         def pin_changed(s):
             if len(s) < len(self.idxs):
                 i = self.idxs[len(s)]
                 addr = self.txdata['address']
-                addr = addr[:i] + '<u><b>' + addr[i:i+1] + '</u></b>' + addr[i+1:]
-                self.addrtext.setHtml(str(addr))
+                if not constants.net.TESTNET:
+                    text = addr[:i] + '<u><b>' + addr[i:i+1] + '</u></b>' + addr[i+1:]
+                else:
+                    # pin needs to be created from mainnet address
+                    addr_mainnet = bitcoin.script_to_address(bitcoin.address_to_script(addr), net=constants.BitcoinMainnet)
+                    addr_mainnet = addr_mainnet[:i] + '<u><b>' + addr_mainnet[i:i+1] + '</u></b>' + addr_mainnet[i+1:]
+                    text = str(addr) + '\n' + str(addr_mainnet)
+                self.addrtext.setHtml(str(text))
             else:
                 self.addrtext.setHtml(_("Press Enter"))
                 
@@ -164,7 +178,7 @@ class LedgerAuthDialog(QDialog):
             if not self.cfg['pair']:
                 self.modes.addItem(_("Mobile - Not paired")) 
             else:
-                self.modes.addItem(_("Mobile - %s") % self.cfg['pair'][1]) 
+                self.modes.addItem(_("Mobile - {}").format(self.cfg['pair'][1]))
         self.modes.blockSignals(False)
         
     def update_dlg(self):
@@ -179,8 +193,8 @@ class LedgerAuthDialog(QDialog):
         self.pinbox.setVisible(self.cfg['mode'] == 0)
         self.cardbox.setVisible(self.cfg['mode'] == 1)
         self.pintxt.setFocus(True) if self.cfg['mode'] == 0 else self.cardtxt.setFocus(True)
-        self.setMaximumHeight(200)
-        
+        self.setMaximumHeight(400)
+
     def do_pairing(self):
         rng = os.urandom(16)
         pairID = (hexlify(rng) + hexlify(hashlib.sha256(rng).digest()[0:1])).decode('utf-8')
@@ -338,11 +352,7 @@ class LedgerWebSocket(QThread):
             ws.send( self.txreq )
             debug_msg("Req Sent", self.txreq)
 
+
 def debug_msg(*args):
     if DEBUG:
         print_msg(*args)        
-
-    
-        
-        
-        

plugins/ledger/qt.py
@@ -1,13 +1,13 @@
-import threading
-
-from PyQt5.Qt import QInputDialog, QLineEdit, QVBoxLayout, QLabel
+#from btchip.btchipPersoWizard import StartBTChipPersoDialog
 
 from electrum_zcash.i18n import _
-from .ledger import LedgerPlugin
-from ..hw_wallet.qt import QtHandlerBase, QtPluginBase
+from electrum_zcash.plugins import hook
+from electrum_zcash.wallet import Standard_Wallet
 from electrum_zcash_gui.qt.util import *
 
-#from btchip.btchipPersoWizard import StartBTChipPersoDialog
+from .ledger import LedgerPlugin
+from ..hw_wallet.qt import QtHandlerBase, QtPluginBase
+
 
 class Plugin(LedgerPlugin, QtPluginBase):
     icon_unpaired = ":icons/ledger_unpaired.png"
@@ -15,6 +15,16 @@ class Plugin(LedgerPlugin, QtPluginBase):
 
     def create_handler(self, window):
         return Ledger_Handler(window)
+
+    @hook
+    def receive_menu(self, menu, addrs, wallet):
+        if type(wallet) is not Standard_Wallet:
+            return
+        keystore = wallet.get_keystore()
+        if type(keystore) == self.keystore_class and len(addrs) == 1:
+            def show_address():
+                keystore.thread.add(partial(self.show_address, wallet, addrs[0]))
+            menu.addAction(_("Show on Ledger"), show_address)
 
 class Ledger_Handler(QtHandlerBase):
     setup_signal = pyqtSignal()
@@ -65,11 +75,7 @@ class Ledger_Handler(QtHandlerBase):
         return 
         
     def setup_dialog(self):
+        self.show_error(_('Initialization of Ledger HW devices is currently disabled.'))
+        return
         dialog = StartBTChipPersoDialog()
         dialog.exec_()
-
-
-        
-        
-        
-        

plugins/trezor/transport.py
@@ -1,0 +1,95 @@
+from electrum_zcash.util import PrintError
+
+
+class TrezorTransport(PrintError):
+
+    @staticmethod
+    def all_transports():
+        """Reimplemented trezorlib.transport.all_transports so that we can
+        enable/disable specific transports.
+        """
+        try:
+            # only to detect trezorlib version
+            from trezorlib.transport import all_transports
+        except ImportError:
+            # old trezorlib. compat for trezorlib < 0.9.2
+            transports = []
+            #try:
+            #    from trezorlib.transport_bridge import BridgeTransport
+            #    transports.append(BridgeTransport)
+            #except BaseException:
+            #    pass
+            try:
+                from trezorlib.transport_hid import HidTransport
+                transports.append(HidTransport)
+            except BaseException:
+                pass
+            try:
+                from trezorlib.transport_udp import UdpTransport
+                transports.append(UdpTransport)
+            except BaseException:
+                pass
+            try:
+                from trezorlib.transport_webusb import WebUsbTransport
+                transports.append(WebUsbTransport)
+            except BaseException:
+                pass
+        else:
+            # new trezorlib.
+            transports = []
+            #try:
+            #    from trezorlib.transport.bridge import BridgeTransport
+            #    transports.append(BridgeTransport)
+            #except BaseException:
+            #    pass
+            try:
+                from trezorlib.transport.hid import HidTransport
+                transports.append(HidTransport)
+            except BaseException:
+                pass
+            try:
+                from trezorlib.transport.udp import UdpTransport
+                transports.append(UdpTransport)
+            except BaseException:
+                pass
+            try:
+                from trezorlib.transport.webusb import WebUsbTransport
+                transports.append(WebUsbTransport)
+            except BaseException:
+                pass
+            return transports
+        return transports
+
+    def enumerate_devices(self):
+        """Just like trezorlib.transport.enumerate_devices,
+        but with exception catching, so that transports can fail separately.
+        """
+        devices = []
+        for transport in self.all_transports():
+            try:
+                new_devices = transport.enumerate()
+            except BaseException as e:
+                self.print_error('enumerate failed for {}. error {}'
+                                 .format(transport.__name__, str(e)))
+            else:
+                devices.extend(new_devices)
+        return devices
+
+    def get_transport(self, path=None):
+        """Reimplemented trezorlib.transport.get_transport,
+        (1) for old trezorlib
+        (2) to be able to disable specific transports
+        (3) to call our own enumerate_devices that catches exceptions
+        """
+        if path is None:
+            try:
+                return self.enumerate_devices()[0]
+            except IndexError:
+                raise Exception("No TREZOR device found") from None
+
+        def match_prefix(a, b):
+            return a.startswith(b) or b.startswith(a)
+        transports = [t for t in self.all_transports() if match_prefix(path, t.PATH_PREFIX)]
+        if transports:
+            return transports[0].find_by_path(path)
+        raise Exception("Unknown path prefix '%s'" % path)